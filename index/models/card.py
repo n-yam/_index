f16d819223160ec79cc602b489eb1de0fc37ea8e
n-yam <n.yamamoto.py@gmail.com>
2024-08-14 01:32:21 +0900

    [Clean] 不要な記述を削除
    不要な記述を削除

--- index/models/card.py
+++ index/models/card.py
@@ -15,14 +15,6 @@ class Card(Base):
     front_text = Column(String(255))
     back_text = Column(String(255))
 
-    @property
-    def frontText(self):
-        return self.front_text
-
-    @property
-    def backText(self):
-        return self.back_text
-
 
 engine = get_engine()
 Base.metadata.create_all(engine)
