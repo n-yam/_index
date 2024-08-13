0a3e65224aabf361353dcf42080f0f5a8ee62021
n-yam <n.yamamoto.py@gmail.com>
2024-08-13 23:24:18 +0900

    [Add] SqlAlchemyを採用
    SqlAlchemyを採用

--- main/wsgi.py
+++ main/wsgi.py
@@ -1,20 +1,44 @@
-from flask import Flask
-from flask import request
+from flask import Flask, request
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.schema import Column
+from sqlalchemy.types import Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
 
 app = Flask(__name__)
+
+engine = create_engine("sqlite:////tmp/index.db")
+
+Base = declarative_base()
+
+
+class Card(Base):
+    __tablename__ = "cards"
+    card_id = Column(Integer, primary_key=True, autoincrement=True)
+    front_text = Column(String(255))
+    back_text = Column(String(255))
+
+
+# Create tables
+Base.metadata.create_all(engine)
 
 
 @app.post("/api/cards")
 def card_post():
-    frontText = request.form["frontText"]
-    backText = request.form["backText"]
+    front_text = request.form["frontText"]
+    back_text = request.form["backText"]
 
-    json = {
-        "frontText": frontText,
-        "backText": backText,
-    }
+    card = Card(front_text=front_text, back_text=back_text)
 
-    return json
+    # Create session
+    SessionClass = sessionmaker(engine)
+    session = SessionClass()
+    session.add(card)
+    session.query(Card).order_by(Card.card_id.desc()).first()
+    session.commit()
+
+    return "OK"
 
 
 def get_app():
