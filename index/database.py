d5250511883d092fb6e926f5bc27d27ed6349eec
n-yam <n.yamamoto.py@gmail.com>
2024-08-14 02:42:05 +0900

    [Fix] CREATE TABLESが多重実行される問題を修正
    Base.metadata.create_all(engine)を1度しか呼ばないように修正

--- index/database.py
+++ index/database.py
@@ -1,14 +1,13 @@
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 
+from index.models.base import Base
+
 engine = create_engine("sqlite:////tmp/index.db")
+Base.metadata.create_all(engine)  # Create tables
 
 
 def get_session():
     SessionClass = sessionmaker(engine)
     session = SessionClass()
     return session
-
-
-def get_engine():
-    return engine
--- index/models/card.py
+++ index/models/card.py
@@ -1,11 +1,7 @@
 from sqlalchemy.schema import Column
 from sqlalchemy.types import Integer, String
-from sqlalchemy.orm import declarative_base
 
-from index.database import get_engine
-
-
-Base = declarative_base()
+from index.models.base import Base
 
 
 class Card(Base):
@@ -14,7 +10,3 @@ class Card(Base):
     id = Column(Integer, primary_key=True, autoincrement=True)
     front_text = Column(String(255))
     back_text = Column(String(255))
-
-
-engine = get_engine()
-Base.metadata.create_all(engine)
