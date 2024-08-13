2823bf44ee0aa24255d8463eb6dab52feebbb6b5
n-yam <n.yamamoto.py@gmail.com>
2024-08-14 00:18:56 +0900

    [Fix] JSON(レスポンス)のキーがスネークケースになっている問題を修正
    キャメルケースに変更

--- tests/test_app.py
+++ tests/test_app.py
@@ -8,16 +8,16 @@ client = app.test_client()
 def test_card_post():
     url = "/api/cards"
 
-    frontText = "THIS IS FRONT TEXT"
-    backText = "THIS IS BACK TEXT"
+    front_text = "THIS IS FRONT TEXT"
+    back_text = "THIS IS BACK TEXT"
 
     formData = {
-        "frontText": frontText,
-        "backText": backText,
+        "frontText": front_text,
+        "backText": back_text,
     }
 
     response = client.post(url, data=formData)
 
     assert response.status_code == 200
-    assert response.json["frontText"] == frontText
-    assert response.json["backText"] == backText
+    assert response.json["frontText"] == front_text
+    assert response.json["backText"] == back_text
