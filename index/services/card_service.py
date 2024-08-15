4e82892fd3f59bcb3e8e7008b2e6b08201af522e
n-yam <n.yamamoto.py@gmail.com>
2024-08-15 15:37:21 +0900

    [Clear] sessionの処理を簡略化
    with文を採用

--- index/services/card_service.py
+++ index/services/card_service.py
@@ -5,87 +5,59 @@ from index import config
 
 class CardService:
     def add(self, card):
-        session = get_session()
-
         try:
-            session.add(card)
-            session.commit()
-            card_saved = session.query(Card).order_by(Card.id.desc()).first()
+            with get_session() as session:
+                session.add(card)
+                session.commit()
+                card_saved = session.query(Card).order_by(Card.id.desc()).first()
+                return card_saved
 
         except Exception as e:
-            session.rollback()
             raise e
-
-        finally:
-            session.close()
-
-        return card_saved
 
     def update(self, newCard):
-        session = get_session()
-
         try:
-            card = session.query(Card).filter_by(id=newCard.id).first()
-            if card:
-                card.front_text = newCard.front_text
-                card.back_text = newCard.back_text
-                session.commit()
-            else:
-                return None
+            with get_session() as session:
+                card = session.query(Card).filter_by(id=newCard.id).first()
+                if card:
+                    card.front_text = newCard.front_text
+                    card.back_text = newCard.back_text
+                    session.commit()
+                    return newCard
+                else:
+                    return None
 
         except Exception as e:
-            session.rollback()
             raise e
-
-        finally:
-            session.close()
-
-        return newCard
 
     def get_all(self):
-        session = get_session()
-
         try:
-            cards = (
-                session.query(Card)
-                .order_by(Card.id.desc())
-                .limit(config.CARD_MAX_LENGTH)
-            )
+            with get_session() as session:
+                cards = (
+                    session.query(Card)
+                    .order_by(Card.id.desc())
+                    .limit(config.CARD_MAX_LENGTH)
+                )
+                return cards
 
         except Exception as e:
             raise e
-
-        finally:
-            session.close()
-
-        return cards
 
     def get(self, id):
-        session = get_session()
-
         try:
-            card = session.get(Card, id)
+            with get_session() as session:
+                card = session.get(Card, id)
+                return card
 
         except Exception as e:
             raise e
-
-        finally:
-            session.close()
-
-        return card
 
     def remove(self, id):
-        session = get_session()
-
         try:
-            deleted_rows_count = session.query(Card).filter(Card.id == id).delete()
-            session.commit()
+            with get_session() as session:
+                rows_count = session.query(Card).filter(Card.id == id).delete()
+                session.commit()
+                return rows_count
 
         except Exception as e:
-            session.rollback()
             raise e
-
-        finally:
-            session.close()
-
-        return deleted_rows_count
