78dbc79cd1aa5701c889ab3dba62fa5073170c41
n-yam <n.yamamoto.py@gmail.com>
2024-08-17 15:44:54 +0900

    [Add] GUI SPAの土台を作成完了
    SPAの土台を作成完了

--- gui/src/index.js
+++ gui/src/index.js
@@ -6,3 +6,43 @@ function component() {
 }
 
 document.body.appendChild(component());
+
+document.querySelectorAll("a").forEach(a => {
+    a.onclick = event => {
+        event.preventDefault();
+        window.history.pushState(null, "", a.href);
+    };
+});
+
+const updateView = () => {
+    const pages = {
+        "/": `
+        <h1>TOP</h1>
+      `,
+        "/hello": `
+        <h1>HELLO</h1>
+      `,
+        "/profile": `
+        <h1>PROFILE</h1>
+      `
+    };
+    const page = pages[window.location.pathname];
+    const render = page || `<h1>404:Not Found<h1>`;
+    document.getElementById("app").innerHTML = render;
+};
+
+document.querySelectorAll("a").forEach(a => {
+    a.onclick = event => {
+        event.preventDefault();
+        window.history.pushState(null, "", a.href);
+        updateView();
+    };
+});
+
+// 初期表示時の処理
+updateView();
+
+// ブラウザバック時の処理
+window.addEventListener("popstate", () => {
+    updateView();
+});
--- gui/webpack.config.js
+++ gui/webpack.config.js
@@ -12,5 +12,6 @@ module.exports = {
         },
         compress: true,
         port: 9000,
+        historyApiFallback: true,
     },
 };
